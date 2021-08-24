182926b54526b926fe82819a5ae0c05660bb8795
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-24 12:18:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let body = document.getElementById("body");
+body.style="margin:0;";
+let mapcontainer = document.createElement("div");
+mapcontainer.setAttribute("id","map");
+mapcontainer.style="height:100vh;width:100vw;";
+body.append(mapcontainer);
+
 mapboxgl.accessToken = 
 'pk.eyJ1IjoiYWxhZWUiLCJhIjoiY2tzbmYzNnl1MDd3eDJwbnA3aTdyajUydSJ9.yMDR9hhKxxn_VF4AJpVfsg';
 navigator.geolocation.getCurrentPosition(successLocation,errorLocation,{
@@ -25,7 +32,3 @@ function setupMap(center){
       });
       map.addControl(directions,"top-left");
 }
-
-
-
-    
